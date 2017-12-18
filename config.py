271a448c77fe888d243a9f4978b0de7a5cb16098
Lucas Zhang <lucasphbs@users.noreply.github.com>
2017-12-18 10:41:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 class Config(object):
     def __init__(self, port=6001):
         self.evaluate_pandas_tickers_lag = 10
-        self.api_key = "92aaed51-3b9c-4ae9-b519-9f2de7a9e013"
-        self.secret_key = "BD5368E9139D8B8C51D2E86EA40EC1B7"
         self.okfuture_api_url = "https://www.okex.com"
